0ac6121959a408badd940249631554728c9975d8
GUILHERME SANTOS NASCIMENTO <guilherme.snascime10@senacsp.edu.br>
2026-05-12 14:04:42 -0300

    atualização

---
+++ app/components/geo.tsx
@@ -0,0 +1,15 @@
+export const Square = () => {
+    return(
+        <div className="w-52 h-52 bg-cyan-400 ">
+            text inspirador da historia do neymar
+        </div>
+    )
+}
+
+export const Circle = () => {
+    return(
+        <div className="w-52 h-52 bg-fuchsia-950 text-white rounded-b-full">
+            Hoje é sexta-feira
+        </div>
+    )
+}
